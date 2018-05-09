584af5fd0d16c63910e372e76775bb88187bc697
zbsccc <chenchengjin@gmail.com>
2018-05-09 22:46:00 +0800

    Update tumblr.module.js
    增加try catch

--- modules/tumblr.module.js
+++ modules/tumblr.module.js
@@ -5,34 +5,45 @@ const fs = require('fs')
 
 const t = {
     getBlogLikes: async (blog, options) => {
-        return new Promise(async (resolve, reject) => {
-            client.blogLikes(blog, options, function (err, data) {
-                if(err){
-                    reject([])
-                }
-                else{
-                    resolve(data)
-                }
-            });
-        })
-    },
-    getVideosList: async () => {
-        return new Promise(async (resolve, reject) => {
-            const file = 'public/files/data.txt';
-            fs.readFile(file, 'utf8', async (err, data) => {
-                if(err){
-                    reject(JSON.stringify(''))
-                }
-                else{
-                    if(data.length > 0){
-                        resolve(data)
+        try{
+            return new Promise(async (resolve, reject) => {
+                client.blogLikes(blog, options, function (err, data) {
+                    if(err){
+                        reject([])
                     }
                     else{
-                        resolve(JSON.stringify(''))
-                    }                    
-                }
-            });
-        })
+                        resolve(data)
+                    }
+                });
+            })
+        }
+        catch (e){
+            console.log(e)
+        }
+    },
+    getVideosList: async () => {
+        try{
+            return new Promise(async (resolve, reject) => {
+                const file = 'public/files/data.txt';
+                fs.readFile(file, 'utf8', async (err, data) => {
+
+                    if(err){
+                        reject(JSON.stringify(''))
+                    }
+                    else{
+                        if(data.length > 0){
+                            resolve(data)
+                        }
+                        else{
+                            resolve(JSON.stringify(''))
+                        }                    
+                    }
+                });
+            })
+        }
+        catch (e){
+            console.log(e)
+        }
     }
 }
 
